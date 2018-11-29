710b37e48ca9a05056911aed24c0a37e3fc54718
Satvik Golechha <f20170117@pilani.bits-pilani.ac.in>
2018-11-29 18:10:30 +0530

    added ip,ether,proto grammars

--- fire.pl
+++ fire.pl
@@ -7,19 +7,28 @@
 
 adapter(a).
 adapter(b).
-adapter(c). 
+adapter(c).
+
+% ***************** Database 2 :: Grammar of protocols allowed ************
+proto('UDP').
+proto('TCP').
+proto('ICMP').
+
+%***************** Database 3 :: Grammar of ethernets allowed************
+ethernet(0x86dd).
+ethernet(0x0800).
+ethernet(0xf002).
+ethernet(0xaa38).
 
 
-% *************************** End of Database 1 ************************************************
+% ***************** Database 4 :: Grammar of ip addresses allowed************
+ip('172.24.16.31').
+ip('192.26.26.250').
+ip('187.24.31.6').
+% *************************** End of Databases ******************************
+
+allow([U,W,Y,Z|_]) :- adapter(U),ethernet(W),\+ip(Y),proto(Z). 
 
 
 
-
-
-%Comment the following samples after writing databases for each clause.
-ethernet(w).
-ip(e).
-protocol(r).
-
-allow(A,B,C,D) :- adapter(A),ethernet(B),ip(C),protocol(D).
 
